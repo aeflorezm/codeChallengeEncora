7574b5fd08e09d054c9748c965104e8b52e77c63
aeflorezm <andresflorez9707@gmail.com>
2022-08-04 02:03:26 -0500

    ammend and test of challenge

--- codeChallenge.js
+++ codeChallenge.js
@@ -28,25 +28,26 @@ const checkRepeatedValues = (tower) => {
  * @param {very_long_type} name           Description.
  * @param {type}           very_long_name Description.
  */
-const minimumAdjacentDifference = (arr, n) => {
+const minCost = (N,H,M) => {
   // Initialising the
   // minimum difference
   //declaring each towers height and costs
-  let hSubI = [];
-  let mSubI = [];
   let finalCost = 0;
-  for (let index = 0; index < n; index++) {
+  //let hSubI = [];
+  //let mSubI = [];
+ /*  
+  for (let index = 0; index < N; index++) {
     if (index % 2 === 0) {
       hSubI.push(arr[index]);
     } else {
       mSubI.push(arr[index]);
     }
   }
-
+  */
   const tower = [];
-  for (let index = 0; index < hSubI.length; index++) {
-    tower[index] = [hSubI[index], mSubI[index]];
-  }
+  for (let index = 0; index < H.length; index++) {
+    tower[index] = [H[index], M[index]];
+  } 
   let positionsRepeatedWithCosts = null;
   do {
     positionsRepeatedWithCosts = checkRepeatedValues(tower);
@@ -73,10 +74,30 @@ const minimumAdjacentDifference = (arr, n) => {
 
 // Driver code
 /* let k = 1; */
-let arr = [3,7,3,8, 10,14]; //test cases on a plain array
+let arr = 
+"3 2 2 3 4 1 5"
+//"3 2 2 2 3 10 6"
+//"4 1 3 2 1000000000 7 3 6 2"
+
+//[3,7,3,8, 10,14]; //test cases on a plain array
 //[2, 4, 2, 1, 3, 5, 3, 6]
 //[2, 4, 2, 1, 3, 5]
 //[3,7,3,8, 10,14];
+var temp =arr.trim('\n').split(/\n|\s/);
+var N = parseInt(temp[0]);
+var H = [];
+for (let index = 1; index < 1+N; index++) {
+  H.push(parseInt(temp[index]));
+  
+}
+var M = [];
+for (let index = 1+N; index < 1+N+N; index++) {
+  M.push(parseInt(temp[index]));
+  
+}
 let n = arr.length;
-let optimizedCost = minimumAdjacentDifference(arr, n);
-console.log(optimizedCost);
+console.log(`N:${N}`)
+console.log(`H:${H}`)
+console.log(`M:${M}`)
+let optimizedCost = minCost(N,H,M);
+console.log(`Optimized cost : ${optimizedCost}`);
